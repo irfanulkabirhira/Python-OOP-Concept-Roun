bc306afbee7871695bfe5db38444cdba38fb0921
MD Irfanul Kabir Hira <106048128+irfanulkabirhira@users.noreply.github.com>
2025-02-12 11:07:56 +0600

    Class object using mian function

--- Class-1/5_class_object_including_main_function.py
+++ Class-1/5_class_object_including_main_function.py
@@ -1,1 +1,15 @@
-class 
+class Student:
+    def __init__(self):
+        self.color = 'blue'
+
+    def hira(self):
+        return "Hello from hira"
+
+def main():
+    obj = Student() # Creating an Object
+    print(obj.color) # Asccesing the instance varibale
+    print(obj.hira()) # Calling the method
+
+# Ensuring the script runs only when executed directly
+if __name__ == "__main__":
+    main()
