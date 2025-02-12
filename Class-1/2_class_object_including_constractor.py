3fcf89135546c803cc4346c6a0e2ad73e0e4239f
MD Irfanul Kabir Hira <106048128+irfanulkabirhira@users.noreply.github.com>
2025-02-12 10:07:44 +0600

    Contractor in python

---
+++ Class-1/2_class_object_including_constractor.py
@@ -0,0 +1,18 @@
+class faculty:
+    # Constractor ==> this will automatically initialize the method
+    def __init__(self):
+        self.id = int(input("Enter Your ID "))
+        self.name = input("Enter Your Name ")
+        self.salary = float(input("Enter your Salary amount "))
+
+    def dispaly(self):
+        print("Faculty ID is = ", self.id)
+        print("Name is = ", self.name)
+        print("Salary is  = ", self.salary)
+
+
+# Creating an object of Faculty class
+a = faculty()
+a.dispaly()
+
+
