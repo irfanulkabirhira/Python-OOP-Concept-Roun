86c8a5032e6c1da657475e7edd928d962cf3936e
MD Irfanul Kabir Hira <106048128+irfanulkabirhira@users.noreply.github.com>
2025-02-15 05:12:46 +0600

    hmm

--- Class-1/4_class_object_for_imprting_another_file.py
+++ Class-1/4_class_object_for_imprting_another_file.py
@@ -3,9 +3,10 @@ class Student:  # Class names should be in PascalCase
         self.color = 'blue'  # Instance variable
 
     def hira(self):  # Added self as the first parameter
-        return
+        return "This is Hira's Fav Color"
 
+# Creating Object of the Class
 obj = Student()  # Creating an instance
 print(obj.color)  # Accessing instance variable correctly
-
+print(obj.hira())
 
