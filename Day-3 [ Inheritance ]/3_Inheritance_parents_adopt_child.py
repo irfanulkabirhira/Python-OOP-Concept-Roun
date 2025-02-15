7745a582e502c36eea50e57701b073a7e3ae8bf6
MD Irfanul Kabir Hira <106048128+irfanulkabirhira@users.noreply.github.com>
2025-02-15 06:35:46 +0600

    Pylint warining in VS code

---
+++ Day-3 [ Inheritance ]/3_Inheritance_parents_adopt_child.py
@@ -0,0 +1,16 @@
+# class Parent:
+#     pass
+# class Child(Parent):
+#     pass
+# class Child2(object):
+#     pass
+
+
+"""
+This module demonstrates inheritance in Python.
+"""
+class Parent:
+    """This class represents a parent in an inheritance example."""
+
+    def __init__(self, name):
+        self.name = name
