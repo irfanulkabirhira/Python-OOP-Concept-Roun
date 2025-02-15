0f816eee1fdb894a86ed01d392ab85677fda17c2
MD Irfanul Kabir Hira <106048128+irfanulkabirhira@users.noreply.github.com>
2025-02-15 06:44:54 +0600

    Why do we use inheritance

---
+++ Day-3 [ Inheritance ]/4_Inheritance_why_use.py
@@ -0,0 +1,18 @@
+"""
+Types of Inheritance
+-> Single
+-> Multiple
+-> Multi-level
+-> heirarchical
+-> hybrid
+
+"""
+
+class employee :
+    pass
+class manager(employee):
+    pass
+class HR(employee):
+    pass
+class programmer(employee):
+    pass 
