73558926a2afb48e026647f7ddebc996565c65e4
MD Irfanul Kabir Hira <106048128+irfanulkabirhira@users.noreply.github.com>
2025-02-15 06:38:18 +0600

    looking for root node

--- Day-3 [ Inheritance ]/3_Inheritance_parents_adopt_child.py
+++ Day-3 [ Inheritance ]/3_Inheritance_parents_adopt_child.py
@@ -1,16 +1,9 @@
-# class Parent:
-#     pass
-# class Child(Parent):
-#     pass
-# class Child2(object):
-#     pass
-
-
-"""
-This module demonstrates inheritance in Python.
-"""
 class Parent:
-    """This class represents a parent in an inheritance example."""
+    pass
+class Child(Parent):
+    pass
+class Child2(object):
+    pass
 
-    def __init__(self, name):
-        self.name = name
+
+
