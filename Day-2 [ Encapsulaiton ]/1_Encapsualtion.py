d5f7747a950f0d69e453a10c6aaf02fe97988345
MD Irfanul Kabir Hira <106048128+irfanulkabirhira@users.noreply.github.com>
2025-02-17 07:11:00 +0600

    dsgd

---
+++ Day-2 [ Encapsulaiton ]/1_Encapsualtion.py
@@ -0,0 +1,22 @@
+# Encapsulation
+
+class Computer:
+    def __init__(self):
+        self.processor = 'Corei-9'
+        self.ram = 64
+        self.ssd = '1 TB '
+    def show(self):
+        print(f"Procesor : {self.processor}\nRam : {self.ram}\nSSD : {self.ssd}")
+
+c1= Computer()
+print("Real Configaration ...")
+c1.show()
+
+class Seller:
+    def __init__(self):
+        c1.processor = 'Corei-5'
+        c1.ram = 16
+        c1.ssd = '512 GB '
+s1 = Seller()
+print("\nAfter Modifying the Configaraiton :")
+c1.show()
