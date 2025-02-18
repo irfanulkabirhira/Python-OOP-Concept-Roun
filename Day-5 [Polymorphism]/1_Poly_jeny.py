36a4fade9cc3eb147cab4253070af870abbfa1fa
MD Irfanul Kabir Hira <106048128+irfanulkabirhira@users.noreply.github.com>
2025-02-18 07:31:41 +0600

    same fuction having multile task

---
+++ Day-5 [Polymorphism]/1_Poly_jeny.py
@@ -0,0 +1,16 @@
+
+'So this is polymorpysm , Same funciton having multiple task'
+
+# way 1 -->
+print("Firstly ", len("Jeny"))
+# way 2 -->
+l1 = ['Hira', 23]
+print("Secondly ",len(l1))
+
+# way 3 -->
+dic1 = {
+    1 : 'Hira',
+    2 : 'Shonal',
+    3 : 'Kapor'
+}
+print("In last ",len(dic1))
