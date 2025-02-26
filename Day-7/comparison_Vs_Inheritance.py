301d8a45fd9938e7faf06cabf2f1d986c28dfc0b
MD Irfanul Kabir Hira <106048128+irfanulkabirhira@users.noreply.github.com>
2025-02-26 16:51:41 +0600

    Comparison vs Inheritance

---
+++ Day-7/comparison_Vs_Inheritance.py
@@ -0,0 +1,27 @@
+'Comparison Vs Inheritance '
+class Engine:
+    def __init__(self , cc):
+        self.capactity = cc
+    def start(self):
+        print("Engine is Started ")
+    def stopped(self):
+        print("Engine is stopped")
+
+
+class Car(Engine):
+    def __init__(self, cc , name):
+        super().__init__(cc)
+        self.name = name
+    def run(self):
+        print ("Car is Running")
+
+c1 = Car(2000, "BMW")
+c1.start()
+c1.run()
+c1.stopped()
+
+'''
+So . is this Actually Logical ??? How is this possoble that car is inherited from Engine ???
+No right !!!
+
+'''
