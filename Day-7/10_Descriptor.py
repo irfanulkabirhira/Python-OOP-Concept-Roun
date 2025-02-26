03f89baf9f914a75a046c26f4317489f0b53cc44
MD Irfanul Kabir Hira <106048128+irfanulkabirhira@users.noreply.github.com>
2025-02-26 18:06:54 +0600

    Descriptor the end

--- Day-7/10_Descriptor.py
+++ Day-7/10_Descriptor.py
@@ -6,13 +6,15 @@ class A :
     def __set__(self, instance , value):
         print("Setting")
         instance.__dict__['Attribite'] = value
-   
+    def __delete__(self, instance):
+        print("Deleting")
+        del instance.__dict__['Attribite']
 
 class myClass:
     Attribite = A()
 
 obj = myClass()
 obj.Attribite = 22
+del obj.Attribite
 print(obj.Attribite)
-
 
